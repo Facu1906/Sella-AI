a54d45f517d496337e39839816d8652ebb2005a6
Facundo Franco <facundofn1906@gmail.com>
2026-03-24 13:35:42 -0300

    Update chatbot.mjs

--- chatbot.mjs
+++ chatbot.mjs
@@ -44,7 +44,7 @@ function detectHumanIntent(msg) {
 }
 
 app.get("/", (req, res) => {
-  res.sendFile(process.cwd() + "/index.html");
+  res.send("Backend is running 🚀");
 });
 
 app.post("/chat", async (req, res) => {
