4ccb2fb1c5ab79d79d2eeab5317c460676ec9290
Facundo Franco <facundofn1906@gmail.com>
2026-03-24 13:01:51 -0300

    clean init

---
+++ storeData.mjs
@@ -0,0 +1,39 @@
+export const products = [
+  {
+    name: "Campera Urban",
+    use_case: "invierno",
+    price: 2990,
+    link: "https://mitienda.com/campera-urban"
+  },
+  {
+    name: "Campera Andes",
+    use_case: "frio intenso",
+    price: 3990,
+    link: "https://mitienda.com/campera-andes"
+  },
+  {
+    name: "Buzo Essential",
+    use_case: "media estacion",
+    price: 2490,
+    link: "https://mitienda.com/buzo-essential"
+  }
+];
+
+export const policies = `
+ENVÍOS
+Hacemos envíos a todo Uruguay.
+Montevideo: hasta 24 horas hábiles.
+Interior: entre 24 y 72 horas hábiles.
+
+CAMBIOS
+Los cambios se pueden realizar dentro de los 30 días con ticket y producto sin uso.
+
+DEVOLUCIONES
+Se aceptan devoluciones dentro de los 30 días.
+
+RETIRO
+Se puede retirar por tienda.
+
+PAGOS
+Aceptamos tarjetas y transferencia bancaria.
+`;
